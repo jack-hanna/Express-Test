fa155aa5cbda26312b5ce7753bd84565270d87e7
jack-hanna <jack_hanna@tamu.edu>
2023-04-13 16:55:59 -0500

    Added Weather

--- public/scripts/Home.js
+++ public/scripts/Home.js
@@ -1,10 +1,10 @@
 /* Script for home page interactive elements
    Developed by
 */
-function updateWeather(){
+function updateWeather(farenheit){
    fetch("https://api.open-meteo.com/v1/forecast?latitude=30.612214183103088&longitude=-96.34101092562297&current_weather=true")
       .then((response) => response.json())
       .then((json) =>
-        {document.getElementById("currWeather").innerHTML = json.current_weather.temperature * 1.8 + 32 + "F"}
+        {document.getElementById("currWeather").innerHTML = (farenheit) ? json.current_weather.temperature * 1.8 + 32 + "F" : json.current_weather.temperature + "C"}
       );
 }
