d5aa70975689310ccf626c597c063cdedfd2f0e5
jack-hanna <jack_hanna@tamu.edu>
2023-04-14 12:20:38 -0500

    Test Whatever

--- app.js
+++ app.js
@@ -22,68 +22,23 @@ app.get("/Manager", (req, res) => {
 });
 
 var catItems_1 = [];
-/*var catItems_2 = ["Items not Retrieved",];
-var catItems_3 = ["Items not Retrieved",];
-var catItems_4 = ["Items not Retrieved",];
-var catItems_5 = ["Items not Retrieved",];
-var catItems_6 = ["Items not Retrieved",];*/
-
-helper.getItemsFromCategory("menuitem", "item", "Feel_Energized_Blend").then(
-  function(value) {
-    value.forEach(function(item){
-      helper.getElement("menuitem", "item", item, "ingredients").then(
-        function(value) {catItems_1 = catItems_1.concat([{name: item, ingredients: value}])},
-        function(error) {catItems_1 = catItems_1.concat([{name: item, ingredients: ["Ingredients not found"]}])}
-      );
-    });
-  },
-  function(error) {catItems_1 += [{name: "Error", ingredients: ["Menu Item not found"]}]}
-);
-
-catItems_1.forEach(function(item){
-  console.log("Name: " + item.name + "    Ingredients: " + item.ingredients);
-});
-
-/*helper.getItemsFromCategory("menuitem", "item", "Get_Fit_Blend").then(
-  function(value) {catItems_2 = value},
-  function(error) {catItems_2 = ["An error has occured"]}
-);
-helper.getItemsFromCategory("menuitem", "item", "Manage_Weight_Blends").then(
-  function(value) {catItems_3 = value},
-  function(error) {catItems_3 = ["An error has occured"]}
-);
-helper.getItemsFromCategory("menuitem", "item", "Be_Well_Blends").then(
-  function(value) {catItems_4 = value},
-  function(error) {catItems_4 = ["An error has occured"]}
-);
-helper.getItemsFromCategory("menuitem", "item", "Enjoy_A_Treat_Blends").then(
-  function(value) {catItems_5 = value},
-  function(error) {catItems_5 = ["An error has occured"]}
-);
-
-var menuItems = [];
-helper.getColumn("menuitem", "item").then(
-  function(value) {menuItems = value},
-  //function(error) {catItems_5 = ["An error has occured"]}
-);
-
-var menu = [];
-menuItems.forEach(function(item){
-  helper.getElement("menuitem", "ingredients").then(
-    function(value) {menu += [{name: item, ingredients: value}]},
-    function(error) {menu += [{name: "Error", ingredients: ["Ingredients not found"]}]}
-  );
-});*/
+var catItems_2 = [];
+var catItems_3 = [];
+var catItems_4 = [];
+var catItems_5 = [];
+var catItems_6 = [];
 
 app.get("/Customer", (req, res) => {
-  res.render('Customer', {
-    catItems_1: catItems_1
-    /*catItems_2: catItems_2,
-    catItems_3: catItems_3,
-    catItems_4: catItems_4,
-    catItems_5: catItems_5,
-    catItems_6: catItems_6*/
-  });
+  loadCustomerMenu().then(
+    res.render('Customer', {
+      catItems_1: catItems_1,
+      catItems_2: catItems_2,
+      catItems_3: catItems_3,
+      catItems_4: catItems_4,
+      catItems_5: catItems_5,
+      catItems_6: catItems_6
+    })
+  );
 });
 
 app.get("/Menu", (req, res) => {
@@ -93,3 +48,67 @@ app.get("/Menu", (req, res) => {
 app.listen(port, () => {
   console.log(`Application is listening on port ${port}!`);
 });
+
+async function loadCustomerMenu(){
+  helper.getItemsFromCategory("menuitem", "item", "Feel_Energized_Blend").then(
+    function(value) {
+      value.forEach(function(item){
+        helper.getElement("menuitem", "item", item, "ingredients").then(
+          function(value) {catItems_1 = catItems_1.concat([{name: item, ingredients: value}])},
+          function(error) {catItems_1 = catItems_1.concat([{name: item, ingredients: ["Ingredients not found"]}])}
+        );
+      });
+    },
+    function(error) {catItems_1 += [{name: "Error", ingredients: ["Menu Item not found"]}]}
+  );
+  
+  helper.getItemsFromCategory("menuitem", "item", "Get_Fit_Blend").then(
+    function(value) {
+      value.forEach(function(item){
+        helper.getElement("menuitem", "item", item, "ingredients").then(
+          function(value) {catItems_2 = catItems_2.concat([{name: item, ingredients: value}])},
+          function(error) {catItems_2 = catItems_2.concat([{name: item, ingredients: ["Ingredients not found"]}])}
+        );
+      });
+    },
+    function(error) {catItems_2 += [{name: "Error", ingredients: ["Menu Item not found"]}]}
+  );
+  
+  helper.getItemsFromCategory("menuitem", "item", "Manage_Weight_Blends").then(
+    function(value) {
+      value.forEach(function(item){
+        helper.getElement("menuitem", "item", item, "ingredients").then(
+          function(value) {catItems_3 = catItems_3.concat([{name: item, ingredients: value}])},
+          function(error) {catItems_3 = catItems_3.concat([{name: item, ingredients: ["Ingredients not found"]}])}
+        );
+      });
+    },
+    function(error) {catItems_3 += [{name: "Error", ingredients: ["Menu Item not found"]}]}
+  );
+  
+  helper.getItemsFromCategory("menuitem", "item", "Be_Well_Blends").then(
+    function(value) {
+      value.forEach(function(item){
+        helper.getElement("menuitem", "item", item, "ingredients").then(
+          function(value) {catItems_4 = catItems_4.concat([{name: item, ingredients: value}])},
+          function(error) {catItems_4 = catItems_4.concat([{name: item, ingredients: ["Ingredients not found"]}])}
+        );
+      });
+    },
+    function(error) {catItems_4 += [{name: "Error", ingredients: ["Menu Item not found"]}]}
+  );
+  
+  helper.getItemsFromCategory("menuitem", "item", "Enjoy_A_Treat_Blends").then(
+    function(value) {
+      value.forEach(function(item){
+        helper.getElement("menuitem", "item", item, "ingredients").then(
+          function(value) {catItems_5 = catItems_5.concat([{name: item, ingredients: value}])},
+          function(error) {catItems_5 = catItems_5.concat([{name: item, ingredients: ["Ingredients not found"]}])}
+        );
+      });
+    },
+    function(error) {catItems_5 += [{name: "Error", ingredients: ["Menu Item not found"]}]}
+  );
+
+  return 0;
+}
